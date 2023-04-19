93e826d662777d2317bf0cb9bf44b3badbd536f7
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-04-19 21:37:11 +0100

    Added images and updated favicon

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import Container from "../Container";
+import Logo from "../navbar/Logo";
 
 function Navbar() {
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
       <div className='py-4 border-b-[1px]'>
         <Container>
-          <h1>Navbar</h1>
+          <div className='flex flex-row justify-between items-center gap-3 md:gap-0'>
+            <Logo />
+          </div>
         </Container>
       </div>
     </div>
