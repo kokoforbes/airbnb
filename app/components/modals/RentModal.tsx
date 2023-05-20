0049e6cef6b5076bb095b77eb68c5f1fdb5483ac
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-05-20 12:34:50 +0100

    Fixed dependency issues

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -69,7 +69,7 @@ const RentModal = () => {
       dynamic(() => import("../Map"), {
         ssr: false,
       }),
-    [location]
+    []
   );
 
   const setCustomValue = (id: string, value: any) => {
